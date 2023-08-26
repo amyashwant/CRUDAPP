6192c3ad87a8a7dcf71de12933a572d3b755f2e1
amyashwant <poonamgangwar083@gmail.com>
2023-08-26 23:28:21 +0530

    fake

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import { BrowserRouter as Router, Routes, Route,Link} from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route,Link} from "react-router-dom";
 
-import AddUser from "./components/addings/AddUser";
+// import AddUser from "./components/addings/AddUser";
 
-import Login from "./components/login/Login";
-import Register from "./components/register/Register";
-import Updates from "./components/updates/Updates";
+// import Login from "./components/login/Login";
+// import Register from "./components/register/Register";
+// import Updates from "./components/updates/Updates";
 
+// function App() {
+//   return (
+//     <div>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Register />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/adduser" element={<AddUser />} />
+//           <Route path="/edituser/:id" element={<Updates />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { useEffect, useState } from "react";
 function App() {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/adduser" element={<AddUser />} />
-          <Route path="/edituser/:id" element={<Updates />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+  const [value, setValue] = useState(1);
 
+  useEffect(() => {
+    setValue(value + 1);
+  }, []);
+
+  return <>value is {value}</>;
+}
 export default App;
